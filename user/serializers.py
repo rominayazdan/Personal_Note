64e022fe96d592c7b09d521e865e3b15ce6f5ac5
Romina <rominayazdann@gmail.com>
2026-05-29 10:09:35 +0330

    Updated project

--- user/serializers.py
+++ user/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from user.models import User
 
+
 class RegisterUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
@@ -13,14 +14,10 @@ class RegisterUserSerializer(serializers.ModelSerializer):
             phone_number=validated_data['phone_number'],
             username=validated_data['username'],
         )
+        user.set_password(validated_data['password'])  # مهم
+        user.save()
 
         return user
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('phone_number', 'username','id')
-        extra_kwargs = {'password': {'write_only': True}}
 
 
 
--- user/views.py
+++ user/views.py
@@ -1,14 +1,14 @@
-from urllib import request
-
-from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
+from rest_framework.permissions import AllowAny
 from rest_framework import status
-from rest_framework import generics
-from user.models import User
 
-from user.serializers import RegisterUserSerializer, UserSerializer
+from user.serializers import RegisterUserSerializer
+
+
+
+
+
 
 class RegisterUserAPI(APIView):
     permission_classes = (AllowAny,)
@@ -17,51 +17,15 @@ class RegisterUserAPI(APIView):
         serializer = RegisterUserSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
-            user.set_password(serializer.validated_data['password'])
-            user.save()
+            return Response(
+                data = {'result':{'user_id' : user.id, 'username' : user.username}},
+                status = status.HTTP_201_CREATED,
+            )
         else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        return Response({"result" : {"user_id" : user.id}})
-
-class ViewUserAPI(generics.ListAPIView):
-    #permission_classes = [IsAuthenticated]
-    permission_classes = (AllowAny,)
-    serializer_class = UserSerializer
-
-
-    def get_queryset(self):
-
-        queryset = User.objects.all()
-        return queryset
-
-class EditUserAPI(generics.RetrieveAPIView):
-    permission_classes = (IsAuthenticated,)
-    serializer_class = UserSerializer
-    lookup_field = 'username'
-
-
-    def get_queryset(self):
-        user = self.request.user
-        queryset = User.objects.filter()
-        return queryset
-
-
-class DeleteUserAPI(generics.DestroyAPIView):
-    permission_classes = (IsAuthenticated,)
-    serializer_class = UserSerializer
-    lookup_field = 'username'
-
-
-    def get_queryset(self):
-        user = self.request.user
-        queryset = User.objects.filter()
-
-        return queryset
-
-
-
-
-
+            return Response(
+                data = {'Error': serializer.errors},
+                status = status.HTTP_400_BAD_REQUEST,
+            )
 
 
 
